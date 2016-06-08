461e1f47a803d78b03c0dfd4d40b55a7d1d75b52
Basile Koko <basile_koko@hotmail.com>
2016-06-08 22:57:15 +0100

    week2_exercise_3e

---
+++ 2_week/3e_method_leap_year.rb
@@ -0,0 +1,34 @@
+# Exercise 3 - Method leap_year
+
+def leap_year?
+  puts 'Enter a year here : '
+  year = gets.chomp.to_i
+  if (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)
+    puts true
+  else
+    puts false
+  end
+end
+leap_year?
+def leap_year2?(year)
+  if (year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)
+    leap_mins = 366 * 24 * 60 * 60
+    puts "The year #{year} has #{leap_mins} minutes."
+  else
+    non_leap_mins = 365 * 24 * 60 * 60
+    puts "The year #{year} has #{non_leap_mins} minutes."
+  end
+end
+leap_year2?(2004)
+
+=begin
+doctest : leap year 
+>> leap_year?
+=> true
+doctest : nonleap year
+>>leap_year
+=> false
+doctest : calculate minutes
+>>leap_year2?(year)
+=> 'string'
+=end
