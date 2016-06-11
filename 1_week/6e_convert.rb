919d6f9bcb0095d34e28477638d44b7e6f7d90ad
Basile Koko <basile_koko@hotmail.com>
2016-06-11 23:06:05 +0100

    Convert Method Cleanup
    Comments are out of date.  We will want to clean this up later.

--- 1_week/6e_convert.rb
+++ 1_week/6e_convert.rb
@@ -1,7 +1,30 @@
-def convert(temperature_in_degree_farhenheit)
-  result = ((temperature_in_degree_farhenheit - 32) * 5/9)*100.round/100.0
-  puts "#{temperature_in_degree_farhenheit} degree Farhenheit equal #{result} degree Celsius."
+# doctest: Convert -40 should return -40
+# >> convert(-40)
+# => -40
+# doctest: Convert 98.6 should return 37
+# >> convert(98.6)
+# => 37
+# doctest: Convert 98 should not return 36
+# >> convert(98).round(6)
+# => 36.666667
+def convert(degree_farhenheit)
+  celsius = (degree_farhenheit - 32.0) * 5/9
+end
+if __FILE__ == $PROGRAM_NAME # Application Guard or Library Guard.
+[-40, 0, 32, 98.6, 98, 100, 212].each do |f_temp|
+  c_temp = convert(f_temp)
+  puts "%0.2fF equal %0.2f degree Celsius." %
+    [c_temp, f_temp,]
+end
 end
 
-convert(50.0)
 
+# Things I like about the current convert method
+# it gets the job done
+# the arguments are well detailed
+# values are returned as we expected
+# Things I don't like about the current convert method
+#arguments are very long
+# complex operation within the method
+# we can do better job at rounding the result
+# lines are too long within the method
