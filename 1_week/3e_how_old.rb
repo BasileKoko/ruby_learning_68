273df60c7088c16d3b72fdef33f28ac1d2657ab9
Victor Goff <keeperotphones@gmail.com>
2016-06-11 20:02:53 -0400

    Using helper methods, to answer how old.
    Reporting is its own method.
    
    Methods do one thing and one thing well.

--- 1_week/3e_how_old.rb
+++ 1_week/3e_how_old.rb
@@ -1,9 +1,26 @@
-def how_old(age_seconds)
-
-  seconds_in_year = 365 * 24 * 60 * 60
-  my_age = age_seconds / seconds_in_year.to_f
-  my_age_format = "%.2f" % my_age
+def seconds_in_year
+  365 * 24 * 60 * 60
 end
 
-puts how_old(979000000)
+# doctest: Calculate the age with seconds
+# >> how_old(979000000).round(6)
+# => 31.043886
+def how_old(seconds)
+  seconds / seconds_in_year.to_f
+end
+
+def report(seconds)
+  format(
+    "If you are #{seconds} seconds old, then you are %.2f years old.",
+    how_old(seconds)
+  )
+end
+
+if __FILE__ == $PROGRAM_NAME
+  # print "How old are you in number of seconds? "
+  # age_in_seconds = gets.to_i
+  [9.79e8, 2.5e8, 2.5e7].each do |age_in_seconds|
+    puts report(age_in_seconds)
+  end
+end
 
