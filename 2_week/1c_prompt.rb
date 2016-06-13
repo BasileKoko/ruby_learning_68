bc739feb411d9d2db6e39cd7e61f49f2de951df0
Basile Koko <basile_koko@hotmail.com>
2016-06-13 20:38:56 +0100

    1c_prompt.rb_to_experiment

--- 2_week/1c_prompt.rb
+++ 2_week/1c_prompt.rb
@@ -1,16 +1,12 @@
 # Challenge 1 week1
 # file called prompt.rb
 
-Promptable
+module Promptable
 def prompt message
   puts message
   answer = gets.chomp
-  answer
 end
 
 name = prompt "What is your name? :>"
-puts name
 puts "Your name is #{name}"
-names << name
-
-puts names
+end
