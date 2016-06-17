5523279c4c742214dfd6a24ab42c9e1845d0e3d6
Victor Goff <keeperotphones@gmail.com>
2016-06-16 20:47:15 -0400

    Doctest set up for text alignment

--- 2_week/3c_column-string_alignment.rb
+++ 2_week/3c_column-string_alignment.rb
@@ -1,13 +1,13 @@
-#doctest: 'split text'
-# >>split (Lorem ipsum dolor sit amet. Consectetur adipiscing elit. Duis auctor in nibh vitae euismod. Nulla facilisi vitae euismod. Aliquam efficitur ullamcorper leo convallis. )
-# =>Line 01 : Lorem ipsum dolor sit amet.
-# =>Line 02 : Consectetur adipiscing elit.
-# =>Line 03 : Duis auctor in nibh vitae euismod.
-# =>Line 04 : Nulla facilisi vitae euismod.
-# =>Line 05 : Aliquam efficitur ullamcorper leo convallis.
+# doctest: Single line
+# >> split("This is a single line\n")
+# => "Line 1: This is a single line\n"
+# doctest: Two lines
+# >> split("This is two lines\nwithout a period on the first line.\n")
+# => "Line 1: This is two lines\nLine 2: without a period on the first line.\n"
 
 def split(text)
-text.split('.').each_with_index do |line, line_num| 
-  puts line_num < 9 ? "Line 0#{line_num.next}: #{line}." : "Line #{line_num.next}: #{line}." 
+text.split('.').each_with_index do |line, line_num|
+  puts line_num < 9 ? "Line 0#{line_num.next}: #{line}." : "Line #{line_num.next}: #{line}."
  end
 end
+
