568f09718e7d7f744aa2fd37c35d72b28b110c0f
Basile Koko <basile_koko@hotmail.com>
2016-06-30 23:37:21 +0100

    week_3

--- 3_week/analyzer.rb
+++ 3_week/analyzer.rb
@@ -9,16 +9,16 @@ string = lines.join
 characters_count = string.size
 characters_count_exclude_whitespace = string.gsub(/\s+/, '').size
 words_count = string.split.size
-sentence_count = string.split(/[\.\!\?]/).size
-paragraph_count = string.split("\n\n").size
-average_number_words_per_sentence = words_count / sentence_count
-average_number_sentence_paragraph = sentence_count / paragraph_count
+sentences_count = string.split(/[\.\!\?]/).size
+paragraphs_count = string.split("\n\n").size
+average_number_words_per_sentence = words_count / sentences_count
+average_number_sentence_paragraph = sentences_count / paragraphs_count
  
-puts "The text analyzed has #{line_count} lines\n"
+puts "The text we analyze has #{line_count} lines\n"
 puts "#{characters_count} charaters\n"
 puts "#{characters_count_exclude_whitespace} characters without whitespace\n"
 puts "#{words_count} words\n"
-puts "#{sentence_count} sentences\n"
-puts "#{paragraph_count} paragrapghs\n"
+puts "#{sentences_count} sentences\n"
+puts "#{paragraphs_count} paragrapghs\n"
 puts "#{average_number_words_per_sentence} average words per sentence\n"
 puts "and #{average_number_sentence_paragraph} average sentences per paragraph"
