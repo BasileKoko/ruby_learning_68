c2405c0cf99b6bcb77e2f511edca3b5c942bc3e1
Basile Koko <basile_koko@hotmail.com>
2016-07-02 12:52:55 +0100

    week_3

--- 3_week/1c_fizz_buzz_extended.rb
+++ 3_week/1c_fizz_buzz_extended.rb
@@ -1,29 +1,40 @@
-#fizz_bizz extended
-
-def print_number 
-  number = rand(1..100)
+class Fizz
+  def initialize(number, output)
+    @number = number     
+    @output = output  
+    
+  end
   
-  if number % 3 == 0 
-    "Fizz" 
-   elsif number % 5 == 0 
-     "Buzz" 
-   elsif number % 3 == 0 && number % 5 == 0 
-     "FizzBuzz" 
-   elsif number % 7 == 0
-     "Dazz"
-   elsif number % 3 == 0 && number % 7 == 0
-     "FizzDazz"
-   elsif number % 3 == 0 && number % 5 == 0 && number % 7 == 0
-     "FizzBuzzDazz"
-   elsif number % 11 == 0
-     "Kezz"
-   elsif number % 2 == 0
-     "Deez"
-   else
-     number
-   end
+  def fizzbuzz
+    yield @number, @output
+  end
 end
 
-puts print_number
+test_fizz = Fizz.new(3, "Fizz")
+test_buzz = Fizz.new(10, "Buzz")
+test_FizzBuzz = Fizz.new(15, "FizzBuzz")
+test_Dazz = Fizz.new(28, "Dazz")
+test_FizzBuzzDazz = Fizz.new(105, "FizzBuzzDazz")
+test_Kezz = Fizz.new(22, "Kezz")
+
+puts test_fizz.fizzbuzz {|number, output| number % 3 == 0? output : number }
+puts test_buzz.fizzbuzz {|number, output| number % 5 == 0? output : number }
+puts test_FizzBuzz.fizzbuzz {|number, output| (number % 3 == 0 && number % 5 == 0)? output : number }
+puts test_Dazz.fizzbuzz {|number, output| number % 7 == 0? output : number }
+puts test_FizzBuzzDazz.fizzbuzz {|number, output| (number % 3 == 0 && number % 5 == 0 && number % 7 == 0)? output : number }
+puts test_Kezz.fizzbuzz {|number, output| number % 11 == 0? output : number }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
