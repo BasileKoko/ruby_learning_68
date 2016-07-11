b8c5d7a1b8ae34a1e56ef51987e06da0ce9f1f70
Victor Goff <keeperotphones@gmail.com>
2016-07-11 19:43:31 -0400

    Code to change existing Rectangle

--- 4_week/2e_rectangle_class.rb
+++ 4_week/2e_rectangle_class.rb
@@ -1,21 +1,23 @@
-
 class Rectangle
-
-  
   def initialize(width, length)
     @width = width
     @length = length
   end
-  
+
   def area
     @width * @length
   end
-  
+
   def perimeter
     (@width + @length) * 2
   end
 end
 
 r = Rectangle.new(23.45, 34.67)
-puts "Area is = #{r.area}" 
-puts "Perimeter is = #{r.perimeter}" 
+puts "Area is = #{r.area}"
+puts "Perimeter is = #{r.perimeter}"
+
+puts 'Notice that this is not a new Rectangle object'
+r.instance_variable_set(:@width, 1)
+puts "Area is = #{r.area}"
+puts "Perimeter is = #{r.perimeter}"
