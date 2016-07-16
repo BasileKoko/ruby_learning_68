ec00638c2fcc5feed2f33c64f515570b32a41217
Basile Koko <basile_koko@hotmail.com>
2016-07-16 13:31:45 +0100

    week_4

--- 4_week/4e_swap_contents.rb
+++ 4_week/4e_swap_contents.rb
@@ -1,6 +1,18 @@
 def file_swap(file_a='file_a.txt', file_b='file_b.txt')
-  File.open(file_a, "w") {|file| file.puts file_b}
-  File.open(file_b, "w") {|file1| file1.puts file_a}
+
+  filea = File.read(file_a)
+  fileb = File.read(file_b)
+
+
+  File.open(file_a, 'w') do |file| 
+    file.puts fileb
+  end
+
+
+  File.open(file_b, 'w') do |file1| 
+    file1.puts filea
+  end 
+
 end
 
 file_swap()
