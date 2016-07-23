20c6ed230eb8a4633910680d8e67d74cbb0be1e1
Basile Koko <basile_koko@hotmail.com>
2016-07-23 10:21:25 +0100

    week_5

--- 5_week/1e_unpredictable_string.rb
+++ 5_week/1e_unpredictable_string.rb
@@ -1,14 +1,17 @@
-class UnpredictableString < String
-  def scramble
-    self.split("").shuffle.join 
+class UnpredictableString
+
+  def initialize(string)
+    @string = string
   end
-  
+
+  def scramble
+    @string.split("").shuffle.join 
+  end
+
   def to_s
-    "#{self.scramble}"
+    scramble
   end
 end
 
 my_phrase = UnpredictableString.new("Now is the time")
 puts my_phrase 
-
-
