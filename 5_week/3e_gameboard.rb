90204f4225a9c2e2d674a44d5c36b5f412be45aa
Basile Koko <basile_koko@hotmail.com>
2016-07-23 18:57:04 +0100

    week_5

---
+++ 5_week/3e_gameboard.rb
@@ -0,0 +1,18 @@
+class GameBoard
+
+  def set_locations_cells(l)
+    @l = l
+  end
+  
+  @@noofhits = 0
+  
+  def check_yourself(user_guess)
+    if @l.include?(user_guess.to_i)
+      puts "Hit"
+     @@noofhits += 1
+     "kill" if @@noofhits == 3
+    else
+     puts "Miss"
+    end 
+  end
+end
