921836ec8a821485442122441b35ef4eb520d0a1
Basile Koko <basile_koko@hotmail.com>
2016-07-27 22:44:15 +0100

    2e_spec_change

--- 5_week/2e_spec_change.rb
+++ 5_week/2e_spec_change.rb
@@ -47,11 +47,3 @@ Rectangle.new('rec'),
 Amoeba.new('amoeba')
 
 ].each(&:click)
-
-=begin
-"There will be an amoeba shape on the screen, with the others. 
-When the user clicks on the amoeba, it will rotate like the others, and play a .hif sound file. 
-The other figures like rectangle rotate around the center. 
-I want the amoeba shape to rotate around a point on one end, like a clock hand !"
-
-=end
