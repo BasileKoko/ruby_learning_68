076d346486aacda05e40dca20298834f3d42763a
Basile Koko <basile_koko@hotmail.com>
2016-08-09 11:07:58 +0100

    week6_exercise_1e_class_person

--- 6_week/1e_class_person.rb
+++ 6_week/1e_class_person.rb
@@ -8,9 +8,11 @@ class Person
   end
 end
 
+#reading instance variable balance
 p1 = Person.new('$100')
 puts p1.balance
 
+#writing instance variable balance
 p2 = Person.new('$300')
 p2.balance = '$250'
 puts p2.balance
