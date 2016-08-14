df8bcc5c6969c852406fd8f03a46726ad56bcfb0
Basile Koko <basile_koko@hotmail.com>
2016-08-14 15:08:35 +0100

    week_6_exercise_5e_game

--- 3_week/6e_deaf_granny_part_1.rb
+++ 3_week/6e_deaf_granny_part_1.rb
@@ -1,3 +1,4 @@
+=begin
 def deaf_granny
   puts "Say something to granny: "
   input = gets.chomp
@@ -13,3 +14,20 @@ def deaf_granny
 end
 
 deaf_granny
+=end
+
+
+deaf_granny?()
+  input = gets.chomp
+  while input !="BYE"
+    if input!= input.upcase
+      puts "HUH?!SPEAK UP, SONNY!"
+      input = gets.chomp
+    else
+      puts "NO,NOT SINCE #{rand(1930..1950)}!"
+      input = gets.chomp
+    end
+  end
+end
+
+puts "Say something to granny: "
